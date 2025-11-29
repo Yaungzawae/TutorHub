84f25c13b437d58168c2311a40651d0d16cb044d
Ye Zaw Aung <yezaw07aung@gmail.com>
2025-11-29 20:15:05 +0700

    Login and register logic

---
+++ functions/student.sql
@@ -0,0 +1,52 @@
+DROP FUNCTION register_student(p_name varchar, p_email varchar, p_password varchar);
+
+CREATE OR REPLACE FUNCTION register_student(
+    p_name VARCHAR,
+    p_email VARCHAR,
+    p_password_hash VARCHAR
+)
+    RETURNS VOID
+    LANGUAGE plpgsql
+    AS $$
+    BEGIN
+        IF EXISTS (SELECT 1 FROM student WHERE email = p_email) THEN
+            RAISE EXCEPTION 'Email already registered';
+        END IF;
+
+        INSERT INTO student (name, email, password_hash)
+        VALUES (p_name, p_email, p_password_hash);
+    END;
+$$;
+
+
+DROP FUNCTION login_student(p_email VARCHAR, p_password_hash VARCHAR);
+
+CREATE OR REPLACE FUNCTION login_student(
+    p_email VARCHAR,
+    p_password_hash VARCHAR
+)
+RETURNS TABLE(
+    id INT,
+    name VARCHAR,
+    email VARCHAR
+)
+LANGUAGE plpgsql
+AS $$
+    BEGIN
+        RETURN QUERY
+        SELECT s.id, s.name, s.email
+        FROM student AS s
+        WHERE s.email = p_email
+        AND password_hash = p_password_hash
+        LIMIT 1;
+
+        IF NOT FOUND THEN
+            RAISE EXCEPTION 'Invalid email or password';
+        END IF;
+
+    END;
+$$;
+
+
+SELECT * from register_student('yza', 'yezawd22.com', 'p1234');
+SELECT * from login_student('yezawd22.com', 'p1234');
